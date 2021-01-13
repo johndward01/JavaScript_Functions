dc285259e849a9d243fbde629df4e7b250ff7a12
johndward01 <johndward01@yahoo.com>
2021-01-13 00:24:40 -0600

    initial commit

--- app.js
+++ app.js
@@ -3,5 +3,28 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+    for (let i = 0; i < count; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(age) {
+    let aboveSixteen = "You can drive!";
+    let belowSixteen = "Sorry, but you need to wait until you're 16";
+
+    if (age < 16) {
+        console.log(belowSixteen);
+    } else {
+        console.log(aboveSixteen)
+    }
+}
+
+let x = function () {
+    checkAge(29)
+}
